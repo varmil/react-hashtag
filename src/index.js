b08ebc4aafc2656d83b2af9c21ecbd5b94c31ac5
Your Name <fkmks247@gmail.com>
2018-07-27 11:03:46 +0900

    remove preact

--- src/index.js
+++ src/index.js
@@ -1,15 +1,13 @@
-import { withFramework } from './hashtag';
+import { withFramework } from './hashtag'
 
 const getParams = function() {
-    try {
-        return [require("react").createElement, true];
-    } catch (err) {
-        try {
-            return [require("preact").h];
-        } catch (err) {
-            console.log("[react-hashtag] there's no react nor preact available to import");
-        }
-    }
-};
+  try {
+    return [require('react').createElement, true]
+  } catch (err) {
+    console.log(
+      "[react-hashtag] there's no react nor preact available to import"
+    )
+  }
+}
 
-export default withFramework.apply(null, getParams());
+export default withFramework.apply(null, getParams())
